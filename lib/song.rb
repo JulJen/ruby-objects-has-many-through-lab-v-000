3d0b974a52d48361a03d93baa315fded35efe16b
Jul Jen-Prog <juliajen.prog@gmail.com>
2018-01-25 18:55:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,16 +8,5 @@ class Song
     @genre = genre
     genre.add_song(self)
   end
-  #
-  # def artist=(artist)
-  #   @artist = artist
-  #   artist.add_song(self)
-  # end
-  #
-  # def genre=(genre)
-  #   @genre = genre
-  #   genre.songs << self if !genre.songs.include?(self)
-  # end
-
 
 end
