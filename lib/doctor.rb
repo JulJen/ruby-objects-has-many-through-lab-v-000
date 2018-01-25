5edc72b7f1f96b54e5859da721f420580eabb305
Jul Jen-Prog <juliajen.prog@gmail.com>
2018-01-25 19:08:34 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -8,19 +8,10 @@ class Doctor
 
   def add_appointment(date)
     @appointments << date
+  end
 
+  def patients
+    @appointments.collect { |scheduled| scheduled.patient }
+  end
 
-end
-
-def initialize(name)
-  @name = name
-  @songs = []
-end
-
-def add_song(song)
-  @songs << song
-end
-
-def artists
-  @songs.collect { |song| song.artist }
 end
